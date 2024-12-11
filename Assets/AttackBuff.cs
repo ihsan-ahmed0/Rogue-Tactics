15a24f8e147d433cb72c261f773b3e0b280889e0
Mattheww31 <49414872+Mattheww31@users.noreply.github.com>
2024-12-10 20:05:27 -0500

    -base abstract class containing key value pair for skills

---
+++ Assets/AttackBuff.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBuff : Skill
+{
+    public int attackMultiplier;
+    public string attackMultType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       
+        setBuff();
+    }
+
+    // Update is called once per frame
+public override void setBuff(){
+     Boost= new KeyValuePair<int,string> (attackMultiplier,attackMultType);
+}
+public int getBuff(int stat){
+    return attackMultiplier*stat;
+}
+}
